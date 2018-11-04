bdb99b301bc45e95435e570f8d297a6b3440e309
Irene R <irene@smallwondermedia.com>
2018-11-04 15:35:41 -0500

    images inside src

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Header from './Components/Header';
 import characters from "./characters.json";
 import Image from "./Components/Image";
 import Wrapper from "./Components/Wrapper";
-import amywong from "../assets/images/amywong.png";
-import bender from "../assets/images/bender.png";
-import farnsworth from "../assets/images/farnsworth.png";
-import fry from "../assets/images/fry.png";
-import kif from "../assets/images/kif.png";
-import leela from "../assets/images/leela.png";
-import nibbler from "../assets/images/nibbler.png";
-import zoidberg from "../assets/images/zoidberg.png";
+import amywong from "./images/amywong.png";
+import bender from "./images/bender.png";
+import farnsworth from "./images/farnsworth.png";
+import fry from "./images/fry.png";
+import kif from "./images/kif.png";
+import leela from "./images/leela.png";
+import nibbler from "./images/nibbler.png";
+import zoidberg from "./images/zoidberg.png";
 
 
 class App extends Component {
