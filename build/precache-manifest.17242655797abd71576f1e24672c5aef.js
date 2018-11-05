cfc8ac31d239d7e6c8153bbf982f867f8057031a
Irene R <irene@smallwondermedia.com>
2018-11-04 20:18:26 -0500

    please work

---
+++ build/precache-manifest.17242655797abd71576f1e24672c5aef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "41e3a2376bc018156a0b441d0ec9370e",
+    "url": "/futurama/static/media/farnsworth.41e3a237.png"
+  },
+  {
+    "revision": "4d3ad664093f27e4b2d0",
+    "url": "/futurama/static/css/main.7781b24d.chunk.css"
+  },
+  {
+    "revision": "539f7f5d9619716caefe",
+    "url": "/futurama/static/js/1.539f7f5d.chunk.js"
+  },
+  {
+    "revision": "3142e3735f515f6f2d21",
+    "url": "/futurama/static/js/runtime~main.3142e373.js"
+  },
+  {
+    "revision": "75558931b5f1b556453b7a83b83864e7",
+    "url": "/futurama/static/media/amywong.75558931.png"
+  },
+  {
+    "revision": "3d20efacfeaf02b55cc8b7b9f1f4d5cd",
+    "url": "/futurama/static/media/fr-bold.3d20efac.ttf"
+  },
+  {
+    "revision": "efb7e8037fbdb3d0ebcdd367f2a9653f",
+    "url": "/futurama/static/media/bender.efb7e803.png"
+  },
+  {
+    "revision": "4d3ad664093f27e4b2d0",
+    "url": "/futurama/static/js/main.4d3ad664.chunk.js"
+  },
+  {
+    "revision": "5a5e91ec2b9ceb2d1c2213f62256740f",
+    "url": "/futurama/static/media/fry.5a5e91ec.png"
+  },
+  {
+    "revision": "0e82fdafa44fb35abab73b3ccff99faa",
+    "url": "/futurama/static/media/kif.0e82fdaf.png"
+  },
+  {
+    "revision": "ae6bd35ea63114051ed845a4b5b986e9",
+    "url": "/futurama/static/media/leela.ae6bd35e.png"
+  },
+  {
+    "revision": "de8daaf502b4dfeb262827222c7910de",
+    "url": "/futurama/static/media/nibbler.de8daaf5.png"
+  },
+  {
+    "revision": "4b988e520571f89c8094d09e37c2a561",
+    "url": "/futurama/static/media/zoidberg.4b988e52.png"
+  },
+  {
+    "revision": "baa98ce7869fb86fe97db6c4e5b032cb",
+    "url": "/futurama/static/media/mars_background1.baa98ce7.jpg"
+  },
+  {
+    "revision": "12b192a691450acf7bd5d16c517374a1",
+    "url": "/futurama/index.html"
+  }
+];
