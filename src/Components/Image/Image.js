3462802929e0a732f3694baa83ebd03f434cc68a
Irene R <irene@smallwondermedia.com>
2018-11-04 20:17:05 -0500

    all amy

--- src/Components/Image/Image.js
+++ src/Components/Image/Image.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import "./Image.css";
+import amywong from "./images/amywong.png";
+import bender from "./images/bender.png";
+import farnsworth from "./images/farnsworth.png";
+import fry from "./images/fry.png";
+import kif from "./images/kif.png";
+import leela from "./images/leela.png";
+import nibbler from "./images/nibbler.png";
+import zoidberg from "./images/zoidberg.png";
 
 const Image = props => (
 
     <span onClick={() => props.handleClick(props.id)}>
         <img className="zoom"
-            src={props.image}
+            src={amywong}
             alt={props.name}
             key={props.id}
         />
