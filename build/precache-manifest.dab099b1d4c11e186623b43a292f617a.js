c970da6140db79f8111e775045ae1e51fa8e6578
Irene R <irene@smallwondermedia.com>
2018-11-04 19:44:41 -0500

    gh-pages yarn

---
+++ build/precache-manifest.dab099b1d4c11e186623b43a292f617a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa98ce7869fb86fe97db6c4e5b032cb",
+    "url": "/futurama/static/media/mars_background1.baa98ce7.jpg"
+  },
+  {
+    "revision": "3d20efacfeaf02b55cc8b7b9f1f4d5cd",
+    "url": "/futurama/static/media/fr-bold.3d20efac.ttf"
+  },
+  {
+    "revision": "3142e3735f515f6f2d21",
+    "url": "/futurama/static/js/runtime~main.3142e373.js"
+  },
+  {
+    "revision": "db7bbddceb9f0e5a99ee",
+    "url": "/futurama/static/js/main.db7bbddc.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/futurama/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "db7bbddceb9f0e5a99ee",
+    "url": "/futurama/static/css/main.c6649c12.chunk.css"
+  },
+  {
+    "revision": "ecbf26e9f2166433e6167bcbc499d2df",
+    "url": "/futurama/index.html"
+  }
+];
