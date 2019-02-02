16e1e5f4c4983dde70bd856f10b210d1cca18346
Irene R <irene@smallwondermedia.com>
2019-02-01 22:09:02 -0500

    refactor header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,14 +4,14 @@ import "./Header.css";
 const Header = (props) => (
 
         <div className="header">
-            <div className="goodNews" id="goodNews">
-                <h2 >Good news, everyone!</h2>
-                <p>Try to click on each character only once. 
-                    <br></br>
-                    If you click on one twice, the score resets.</p>
-                    <br></br>
-                    <div className="score" id="score">Score: {props.score}</div>
-            </div>
+
+            <h2 >Good news, everyone!</h2>
+            <p>Try to click on each character only once. 
+                <br></br>
+                If you click on one twice, the score resets.
+            </p>
+                <br></br>
+            <div className="score">Score: {props.score}</div>
 
         </div>
 
